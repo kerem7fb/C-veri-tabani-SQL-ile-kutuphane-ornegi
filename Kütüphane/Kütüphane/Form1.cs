0fa69cc71f7fe204f19aad2fbc8987d5111409d6
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2024-12-24 07:23:21 +0300

    Add files via upload

---
+++ Kütüphane/Kütüphane/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace Kütüphane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection yeni = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\user\\Desktop\\10-M\\Kütüphane\\Veri Tabanı.mdb");
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            OleDbDataAdapter adtr = new OleDbDataAdapter("Select * From Kullanıcı_Adı",yeni);
+            DataTable dt = new DataTable();
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            yeni.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 yeni = new Form2();
+            yeni.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Kütüphane/Kütüphane/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace Kütüphane
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection yeni = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Users\\user\\Desktop\\10-M\\Kütüphane\\Veri Tabanı.mdb");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            OleDbDataAdapter adtr = new OleDbDataAdapter("Select * From Ürünler", yeni);
+            DataTable dt = new DataTable();
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            yeni.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form1 geri = new Form1();
+            geri.Show();
+            this.Hide();
+        }
+    }
+}
